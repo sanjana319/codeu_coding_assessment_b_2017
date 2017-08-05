004ce0af2d821d13111a62a291186e13ccdc590d
Sanjana Natraj <snatraj@ucsc.edu>
2017-08-04 23:53:11 -0700

    implemented mytokenreader

--- src/com/google/codeu/mathlang/impl/MyTokenReader.java
+++ src/com/google/codeu/mathlang/impl/MyTokenReader.java
@@ -27,9 +27,14 @@ import com.google.codeu.mathlang.parsing.TokenReader;
 // work with the test of the system.
 public final class MyTokenReader implements TokenReader {
 
+  private final String source;
+  private int position;
+
   public MyTokenReader(String source) {
     // Your token reader will only be given a string for input. The string will
     // contain the whole source (0 or more lines).
+    this.source = source;
+    position = 0;
   }
 
   @Override
@@ -41,6 +46,72 @@ public final class MyTokenReader implements TokenReader {
     // If for any reason you detect an error in the input, you may throw an IOException
     // which will stop all execution.
 
-    return null;
+    String curr = source.substring(position);
+    
+    if(curr.length() > source){
+      return null;
+    }
+
+    char token = Character.toLowerCase(curr.charAt(0));
+
+    if(token == '"'){
+      int lastPosition = curr.indexOf('"', 1);
+      int temp = -1;
+      if(lastPosition != temp){
+        position += lastPosition + 1;
+        return new StringToken(position.substring(1, lastPosition));
+      } else {
+        throw new IOException("No ending quote in the string");
+      }
+    }
+    
+    else if(token != ';'){
+      String name = "" + current.charAt(0);
+      int i = 1;
+      for(i; i < curr.length() && isAlphabeticOrNumeric(curr.charAt(i)); i++){
+        name += current.charAt(i);
+      }
+      position += i;
+      return new NameToken(name);
+    }
+    
+    else if(Character.isDigit(token)){
+      String num = "" + token;
+      int i = 1;
+      for(i; curr.length() && Character.isDigit(curr.charAt(i); i++)) {
+        num += curr.charAt(i);
+      }
+      position += i;
+      return new NumberToken(Double.parseDouble(num));
+    }
+    
+    else{
+      position++;
+      return new SymbolToken(';');
+    }
+  } //end next
+
+  public static boolean isAlphabeticOrNumeric(char token){
+    if(Character.isAlphabetic(token)){
+      return true;
+    }
+    else if(Character.isDigit(token)){
+      return true;
+    }
+    return false;
   }
+
+  public static boolean isSymbol(char token){
+    if(token == '-'){
+      return true;
+    }
+    else if(token == '='){
+      return true;
+    }
+    else if(token == '+'){
+      return true;
+    }
+    return false;
+  }
+
 }
